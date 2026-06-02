8a21b255457c5081d766ab770e0b380986afc683
William Rogan <william.rogan@outlook.de>
2026-06-02 17:24:22 +0200

    Add icons for information

--- web/src/components/registration.vue
+++ web/src/components/registration.vue
@@ -3,93 +3,115 @@
 <template>
   <div class="container">
     <h2>Informationen</h2>
-    <div class="row">
-      <div class="col-3 text-end"><strong>Start</strong></div>
-      <div class="col-9 text-start">Postamt, Nähe Stadtplatz Mattighofen</div>
-    </div>
-    <div class="row">
-      <div class="col-3 text-end"><strong>Ziel</strong></div>
-      <div class="col-9 text-start">Stadtplatz Mattighofen</div>
-    </div>
-    <div class="row">
-      <div class="col-3 text-end"><strong>Umkleiden/Duschen</strong></div>
-      <div class="col-9 text-start">Sepp Öller Halle Mattighofen</div>
-    </div>
-    <div class="row">
-      <div class="col-3 text-end"><strong>Parkmöglichkeiten</strong></div>
-      <div class="col-9 text-start">Sepp Öller Halle, Parkplatz Wasseracker und Musikschule</div>
-    </div>
-    <div class="row">
-      <div class="col-3 text-end"><strong>Infos und Anmeldung</strong></div>
-      <div class="col-9 text-start">
-        <a href="https://www.mattighofener-city-run.at">https://www.mattighofener-city-run.at</a>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-3 text-end"><strong>Startnummerausgabe</strong></div>
-      <div class="col-9 text-start">Sparkasse, Stadtplatz 39</div>
-    </div>
-    <div class="row">
-      <div class="col-3 text-end"><strong>Nennschluss</strong></div>
-      <div class="col-9 text-start">
-        Freitag, 24.04.2026, 13:00 Uhr<br />Nachmeldung bis 45 Minuten vor Start möglich (Sparkasse)
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-3 text-end"><strong>Nenngeld</strong></div>
-      <div class="col-9 text-start">
-        <div class="col">
-          <div class="row">
-            <div class="col-3">U6 bis U14</div>
-            <div class="col">&euro; 7,-</div>
+    <div class="row justify-content-center">
+      <div class="col lg-2 col-md-8 col-sm-12">
+        <div class="container">
+          <div class="row pb-2">
+            <div class="col-1 text-end">
+              <font-awesome-icon :icon="['fas', 'person-running']" />
+            </div>
+            <div class="col-11 text-start">Postamt, Nähe Stadtplatz Mattighofen</div>
           </div>
-          <div class="row">
-            <div class="col-3">U16/U20</div>
-            <div class="col">&euro; 10,-</div>
+          <div class="row pb-2">
+            <div class="col-1 text-end">
+              <font-awesome-icon :icon="['fas', 'flag-checkered']" />
+            </div>
+            <div class="col-11 text-start">Stadtplatz Mattighofen</div>
           </div>
-          <div class="row">
-            <div class="col-3">EWS Energy Fun Run</div>
-            <div class="col">&euro; 15,-</div>
+          <div class="row pb-2">
+            <div class="col-1 text-end"><font-awesome-icon :icon="['fas', 'shower']" /></div>
+            <div class="col-11 text-start">Sepp Öller Halle Mattighofen</div>
           </div>
-          <div class="row">
-            <div class="col-3">Nordic Walking</div>
-            <div class="col">&euro; 15,-</div>
+          <div class="row pb-2">
+            <div class="col-1 text-end">
+              <font-awesome-icon :icon="['fas', 'square-parking']" />
+            </div>
+            <div class="col-11 text-start">
+              Sepp Öller Halle, Parkplatz Wasseracker und Musikschule
+            </div>
           </div>
-          <div class="row">
-            <div class="col-3">City Run</div>
-            <div class="col">&euro; 20,-</div>
+          <div class="row pb-2">
+            <div class="col-1 text-end">
+              <font-awesome-icon :icon="['fas', 'circle-question']" />
+            </div>
+            <div class="col-11 text-start">
+              <a href="https://www.mattighofener-city-run.at"
+                >https://www.mattighofener-city-run.at</a
+              >
+            </div>
           </div>
-          <div class="row">
-            <div class="col-12 text-start">
-              Nachmeldegebühr am Wettkampftag: +&euro; 5,- U6 bis U14 keine Nachmeldegebühr<br />
-              Ein Teil des Reinerlöses geht an die Lebenshilfe Mattighofen.
+          <div class="row pb-2">
+            <div class="col-1 text-end"><font-awesome-icon :icon="['fas', 'id-card']" /></div>
+            <div class="col-11 text-start">Sparkasse, Stadtplatz 39</div>
+          </div>
+          <div class="row pb-2">
+            <div class="col-1 text-end"><font-awesome-icon :icon="['fas', 'calendar-days']" /></div>
+            <div class="col-11 text-start">
+              Freitag, 24.04.2026, 13:00 Uhr<br />Nachmeldung bis 45 Minuten vor Start möglich
+              (Sparkasse)
+            </div>
+          </div>
+          <div class="row pb-2">
+            <div class="col-1 text-end"><font-awesome-icon :icon="['fas', 'euro-sign']" /></div>
+            <div class="col-11 text-start">
+              <div class="col">
+                <div class="row">
+                  <div class="col-4">U6 bis U14</div>
+                  <div class="col">&euro; 7,-</div>
+                </div>
+                <div class="row">
+                  <div class="col-4">U16/U20</div>
+                  <div class="col">&euro; 10,-</div>
+                </div>
+                <div class="row">
+                  <div class="col-4">EWS Energy Fun Run</div>
+                  <div class="col">&euro; 15,-</div>
+                </div>
+                <div class="row">
+                  <div class="col-4">Nordic Walking</div>
+                  <div class="col">&euro; 15,-</div>
+                </div>
+                <div class="row">
+                  <div class="col-4">City Run</div>
+                  <div class="col">&euro; 20,-</div>
+                </div>
+                <div class="row pt-2">
+                  <div class="col-12 text-start">
+                    Nachmeldegebühr am Wettkampftag: +&euro; 5,- U6 bis U14 keine Nachmeldegebühr<br />
+                    Ein Teil des Reinerlöses geht an die Lebenshilfe Mattighofen.
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="row pb-2">
+            <div class="col-1 text-end"><font-awesome-icon :icon="['fas', 'stopwatch']" /></div>
+            <div class="col-11 text-start">
+              <p>Time2Win - Chipzeitnehmung (Startnummer)</p>
+              <p>
+                Ergebnislisten und Urkunden direkt nach der Veranstaltung unter
+                <a href="https://www.tsvmattighofen.at/laufen" target="_blank"
+                  >www.tsvmattighofen.at</a
+                >
+                oder
+                <a href="https://time2win.at/event/966/results" target="_blank">www.time2win.at</a>.
+              </p>
+            </div>
+          </div>
+          <div class="row pb-2">
+            <div class="col-1 text-end"><font-awesome-icon :icon="['fas', 'section']" /></div>
+            <div class="col-11 text-start">
+              Turn- und Sportverein Mattighofen. Für Unfälle aller Art, auch Dritten gegenüber, wird
+              nicht gehaftet
+            </div>
+          </div>
+          <div class="row pb-2">
+            <div class="col-1 text-end"><font-awesome-icon :icon="['fas', 'info']" /></div>
+            <div class="col-11 text-start">
+              Der Lauf zählt zum Grenzlandcup und österreichischen Volkslaufcup
             </div>
           </div>
         </div>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-3 text-end"><strong>Zeitnehmung</strong></div>
-      <div class="col-9 text-start">
-        <p>Time2Win - Chipzeitnehmung (Startnummer)</p>
-        <p>
-          Ergebnislisten und Urkunden direkt nach der Veranstaltung unter
-          <a href="https://www.tsvmattighofen.at/laufen" target="_blank">www.tsvmattighofen.at</a>
-          oder <a href="https://time2win.at/event/966/results" target="_blank">www.time2win.at</a>.
-        </p>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-3 text-end"><strong>Veranstalter/Haftung</strong></div>
-      <div class="col-9 text-start">
-        Turn- und Sportverein Mattighofen. Für Unfälle aller Art, auch Dritten gegenüber, wird nicht
-        gehaftet
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-3 text-end"><strong>Allgemeine Informationen</strong></div>
-      <div class="col-9 text-start">
-        Der Lauf zählt zum Grenzlandcup und österreichischen Volkslaufcup
       </div>
     </div>
     <div class="mb-5">&nbsp;</div>
--- web/src/main.ts
+++ web/src/main.ts
@@ -2,9 +2,39 @@ import "bootstrap/dist/js/bootstrap.bundle.min.js"
 import { library } from "@fortawesome/fontawesome-svg-core"
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome"
 
-import { faPersonRunning, faPersonHiking, faAngleUp } from "@fortawesome/free-solid-svg-icons"
+import {
+  faPersonRunning,
+  faPersonHiking,
+  faAngleUp,
+  faLocationPin,
+  faFlagCheckered,
+  faShower,
+  faSquareParking,
+  faIdCard,
+  faInfo,
+  faCalendarDays,
+  faEuroSign,
+  faStopwatch,
+  faSection,
+  faCircleQuestion,
+} from "@fortawesome/free-solid-svg-icons"
 
-library.add(faPersonRunning, faPersonHiking, faAngleUp)
+library.add(
+  faPersonRunning,
+  faPersonHiking,
+  faAngleUp,
+  faLocationPin,
+  faFlagCheckered,
+  faShower,
+  faSquareParking,
+  faIdCard,
+  faInfo,
+  faCalendarDays,
+  faEuroSign,
+  faStopwatch,
+  faSection,
+  faCircleQuestion,
+)
 
 import "./assets/main.css"
 
